a8cd0e3ae51fa9a35ffec7eccd5af46c343811e3
Warda Bibi <wardabibi14@gmail.com>
2025-12-31 14:14:38 +0500

    Otimized approach for challenge 2

--- Challenge2_MinWindow.py
+++ Challenge2_MinWindow.py
@@ -23,3 +23,42 @@ def MinWindow(log, pattern):
 log = input("log=").strip()
 pattern = input("pattern=").strip()
 print(MinWindow(log, pattern))
+
+
+#optimized approach
+from collections import Counter
+
+def MinWindow(log, pattern):
+    if not log or not pattern:
+        return ""
+    
+    need = Counter(pattern)
+    window = {}
+    
+    have = 0
+    required = len(need)
+    left = 0
+    min_len = float('inf')
+    result = ""
+    
+    for right in range(len(log)):
+        char = log[right]
+        window[char] = window.get(char, 0) + 1
+        
+        if char in need and window[char] == need[char]:
+            have += 1
+        
+        while have == required:
+            if right - left + 1 < min_len:
+                min_len = right - left + 1
+                result = log[left:right+1]
+            
+            window[log[left]] -= 1
+            if log[left] in need and window[log[left]] < need[log[left]]:
+                have -= 1
+            left += 1
+    
+    return result
+log = input("log=").strip()
+pattern = input("pattern=").strip()
+print(MinWindow(log, pattern))
