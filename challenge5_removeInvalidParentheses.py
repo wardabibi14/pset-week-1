d25f6b79b0a3654240806dde352b4211be1ccb33
Warda Bibi <wardabibi14@gmail.com>
2025-12-31 15:08:51 +0500

    Optimized approach for challenge 5

--- challenge5_removeInvalidParentheses.py
+++ challenge5_removeInvalidParentheses.py
@@ -34,3 +34,57 @@ def removeInvalidParentheses(expr):
     dfs(expr, 0, 0)
     return list(res)
 
+expr = input("Enter expression: ")
+result = removeInvalidParentheses(expr)
+print(result)
+
+
+
+#Optimized approach
+def countRemovals(s):
+    left = right = 0
+    for ch in s:
+        if ch == '(':
+            left += 1
+        elif ch == ')':
+            if left > 0:
+                left -= 1
+            else:
+                right += 1
+    return left, right
+def removeInvalidParentheses(expr):
+    left_rem, right_rem = countRemovals(expr)
+    res = set()
+
+    def dfs(index, path, balance, l_rem, r_rem):
+        # Invalid state
+        if balance < 0 or l_rem < 0 or r_rem < 0:
+            return
+
+        # End of string
+        if index == len(expr):
+            if balance == 0 and l_rem == 0 and r_rem == 0:
+                res.add(path)
+            return
+
+        ch = expr[index]
+
+        if ch == '(':
+            dfs(index + 1, path, balance, l_rem - 1, r_rem)
+            dfs(index + 1, path + ch, balance + 1, l_rem, r_rem)
+
+        elif ch == ')':
+            dfs(index + 1, path, balance, l_rem, r_rem - 1)
+            dfs(index + 1, path + ch, balance - 1, l_rem, r_rem)
+
+        else:
+
+            dfs(index + 1, path + ch, balance, l_rem, r_rem)
+
+    dfs(0, "", 0, left_rem, right_rem)
+    return list(res)
+
+expr = input("Enter expression: ")
+result = removeInvalidParentheses(expr)
+print(result)
+
